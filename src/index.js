dec1af9c43f3743f0d544034023f2097cf0bea6d
freeway26tw <freeway26tw@gmail.com>
2023-04-27 18:56:25 +0800

    feat: Slash Command Options and Choices

--- src/index.js
+++ src/index.js
@@ -17,12 +17,10 @@ client.on('ready', (c) => {
 client.on('interactionCreate', (interaction) => {
   if (!interaction.isChatInputCommand()) return
 
-  if (interaction.commandName === 'hey') {
-    return interaction.reply('hey!')
-  }
-
-  if (interaction.commandName === 'ping') {
-    return interaction.reply('pong!')
+  if (interaction.commandName === 'add') {
+    const num1 = interaction.options.get('first-number').value
+    const num2 = interaction.options.get('second-number').value
+    interaction.reply(`The sum is ${num1 + num2}`)
   }
 })
 
--- src/register-commands.js
+++ src/register-commands.js
@@ -1,10 +1,38 @@
 require('dotenv').config()
-const { REST, Routes } = require('discord.js')
+const { REST, Routes, ApplicationCommandOptionType } = require('discord.js')
 
 const commands = [
   {
-    name: 'hey',
-    description: 'Replies with hey!',
+    name: 'add',
+    description: 'Adds two numbers.',
+    options: [
+      {
+        name: 'first-number',
+        description: 'The first number.',
+        type: ApplicationCommandOptionType.Number,
+        choices: [
+          {
+            name: 'one',
+            value: 1
+          },
+          {
+            name: 'two',
+            value: 2
+          },
+          {
+            name: 'three',
+            value: 3
+          },
+        ],
+        required: true
+      },
+      {
+        name: 'second-number',
+        description: 'The second number.',
+        type: ApplicationCommandOptionType.Number,
+        required: true
+      }
+    ]
   },
   {
     name: 'ping',
